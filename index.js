1c9857ccc7e7eb8fcca73288e585c1658590033a
Ducceschi Nino <nino.duduch@gmail.com>
2026-09-10 12:56:03 +0200

    feat: utilisable lib faker

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-console.log("bonjour npm".toUpperCase())
-
 import dayjs from "dayjs";
 
+import { fakerFR as faker } from "@faker-js/faker";
+
+console.log("bonjour npm".toUpperCase());
+
 console.log(dayjs().format("DD/MM/YYYY"));
+
+console.log(faker.person.fullName());
+console.log(faker.internet.email());
